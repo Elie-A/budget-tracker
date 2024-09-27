2031712905423a62e9b6cd3ce7586fe4054eee43
Elie-A <elieabdallah961@gmail.com>
2024-09-27 17:49:52 +0200

    finalized manage page

--- app/(dashboard)/_actions/categories.ts
+++ app/(dashboard)/_actions/categories.ts
@@ -4,6 +4,8 @@ import prisma from "@/lib/prisma";
 import {
   CreateCategorySchema,
   CreateCategorySchemaType,
+  DeleteCategorySchema,
+  DeleteCategorySchemaType,
 } from "@/schema/categories";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -28,4 +30,28 @@ export async function CreateCategory(form: CreateCategorySchemaType) {
       type,
     },
   });
+}
+
+export async function DeleteCategory(form: DeleteCategorySchemaType) {
+  const parsedBody = DeleteCategorySchema.safeParse(form);
+  if (!parsedBody.success) {
+    throw new Error("Bad request");
+  }
+
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-up");
+  }
+
+  const categories = await prisma.category.delete({
+    where: {
+      name_userId_type: {
+        userId: user.id,
+        name: parsedBody.data.name,
+        type: parsedBody.data.type,
+      },
+    },
+  });
+
+  return categories;
 }
